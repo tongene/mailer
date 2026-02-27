941ea74624bf00da575346e68715560ee5a21cdf
tongene <ngene.christina@gmail.com>
2026-02-27 20:42:14 +0000

    deploy-1.2/27

--- index.ts
+++ index.ts
@@ -51,7 +51,7 @@ fastify.post('/admin/send-newsletter', async (req, reply ) => {
   const { campaigns } = req.body as { campaigns: CampaignProps[] }
 
   const { data } = await fastify.supabase
-    .from('newsletter_js_2')
+    .from('newsletter_subscribers')
     .select('email, name')
     .eq("unsubscribed", false);
     for (const user of data??[]) {
@@ -84,23 +84,30 @@ const unsubscribeUrl = `https://culturays.com/api/unsubscribed?email=${user.emai
        <h2 style="color:#2c3e50;">Today's Top Stories</h2> 
     <div style="max-width: 600px; margin: auto; font-family: Arial, sans-serif; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0,0,0,0.05);">
         
+    <p style="font-size:18px;color: #6677; text-align:center">
+    Get Your Information Right on. Read Only Verified News.
+    <br/>
+    <a href="https://culturays.com" style="color:#f97316;">
+      Culturays — The Urban Naija News
+    </a>
+  </p>
        <p style="font-size: 35px; color: #2c3e50; margin: 10px 0; font-weight: bold;">Have you seen our AI Aid?<a href="https://culturays.com/naija-events/" style="display:inline-block;margin-top:1em;padding:0.5em 1em;background:#0070f3;color:white;text-decoration:none;border-radius:5px;">Try it!</a></p>
        
-       <hr style="margin: 40px 0; border: none; border-top: 1px solid #eaeaea;" />    
+       <hr style="margin: 40px 0; border: none; border-top: 1.5px solid #0c0f8f;" />    
          <p style="font-size: 16px; color: #333333; text-transform: capitalize;">Hi ${user?.name},</p>
       ${postsHtml}
     <p style="margin-top: 30px; font-size: 15px; color: #333333;">
           Warm regards,<br>
           <strong>Urban Naija</strong>
         </p>
-       <hr style="margin: 40px 0; border: none; border-top: 1px solid #eaeaea;" />        
+       <hr style="margin: 40px 0; border: none; border-top: 1px solid #0c0f8f;" />        
        <h2 style="font-size: 22px; color: #2c3e50; margin: 10px 0;">News Made for You</h2>
 
         <img src="https://culturays.com/tinitasks-poster.JPG/" alt="Tini Tasks Poster" style="width: 100%; border-radius: 6px; margin-bottom: 20px;" />
 
         <p><a href="https://gowork.africareinvented.com/" style="display:inline-block;margin-top:1em;padding:0.5em 1em;background:#0070f3;color:white;text-decoration:none;border-radius:5px;">Connect. Collaborate. Conquer on Tini Tasks</a></p>
 
-        <hr style="margin: 40px 0; border: none; border-top: 1px solid #eaeaea;" />
+        <hr style="margin: 40px 0; border: none; border-top: 1px solid #0c0f8f;" />
       
        <footer style="font-size: 13px; color: #999999; text-align: center;"> 
   <hr style="margin-top:30px" />
